d2991ebb77065212e4ef52152eda1c55e71e84e7
vanisri1525 <vaninavadeep22@gmail.com>
2025-04-23 05:24:32 +0530

    Vani's conflict resolved

--- src/test/java/org/base/BaseClass.java
+++ src/test/java/org/base/BaseClass.java
@@ -13,9 +13,7 @@ public class BaseClass {
 		System.out.println("Test6");	
 		System.out.println("Test7");
 		System.out.println("Test8");
-		System.out.println("Test9");
-		System.out.println("Test10");
-			}
+		}
 
 	}
 
