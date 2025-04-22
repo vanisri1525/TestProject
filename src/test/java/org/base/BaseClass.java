ad5327f0f26df76a1a92263a62a2813e9bc4ea7b
vanisri1525 <vaninavadeep22@gmail.com>
2025-04-23 04:38:31 +0530

    vani's code

--- src/test/java/org/base/BaseClass.java
+++ src/test/java/org/base/BaseClass.java
@@ -11,10 +11,8 @@ public class BaseClass {
 		System.out.println("Test4");
 		System.out.println("Test5");
 		System.out.println("Test6");
-		System.out.println("Test7");
-		System.out.println("Test8");
-		System.out.println("Test9");
-		
+
+
 	}
 	}
 
