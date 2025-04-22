a081c56e73564f37925978ce1f5266a35de56405
vanisri1525 <vaninavadeep22@gmail.com>
2025-04-23 00:59:16 +0530

    vani's commit

--- src/test/java/org/base/BaseClass.java
+++ src/test/java/org/base/BaseClass.java
@@ -9,6 +9,8 @@ public class BaseClass {
 		System.out.println("Test2");
 		System.out.println("Test3");
 		System.out.println("Test4");
+		System.out.println("Test5");
+		System.out.println("Test6");
 
 
 
