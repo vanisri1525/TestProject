97125f5298a3f5b58753a7b543eaee69c567dccb
vanisri1525 <vaninavadeep22@gmail.com>
2025-04-22 22:35:34 +0530

    First Commit

--- src/test/java/org/base/BaseClass.java
+++ src/test/java/org/base/BaseClass.java
@@ -1,8 +1,14 @@
 package org.base;
 
+import org.openqa.selenium.WebDriver;
+
 public class BaseClass {
+	//public static WebDriver driver;
 	public static void main(String[] args) {
-		System.out.println("Test 1");
+		System.out.println("Test1");
+		System.out.println("Test2");
+
+		
 	}
 	}
 
